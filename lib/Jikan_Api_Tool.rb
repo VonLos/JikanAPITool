4962c38d9a367d8da1e8bff49c4b5e25d2d4facd
rylandgrounds <rylandgrounds@gmail.com>
2020-05-28 16:14:08 -0500

    first commit

---
+++ lib/Jikan_Api_Tool.rb
@@ -0,0 +1,6 @@
+require "Jikan_Api_Tool/version"
+
+module JikanApiTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Jikan_Api_Tool/version.rb
@@ -0,0 +1,3 @@
+module JikanApiTool
+  VERSION = "0.1.0"
+end
