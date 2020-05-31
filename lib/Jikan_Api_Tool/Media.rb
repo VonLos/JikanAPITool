0c3ae6b5d705ed630ec5f8d6c95b238d8c6e8cd2
rylandgrounds <rylandgrounds@gmail.com>
2020-05-31 14:55:19 -0500

    created media class and required it's file

---
+++ lib/Jikan_Api_Tool/Media.rb
@@ -0,0 +1,13 @@
+class Media
+attr_accessor :mal_id, :title, :rated, :synopsis
+@all = []
+def initialize(mal_id= nil, title = nil, rated = nil, synopsis =nil)
+@mal_id = mal_id
+@title = title
+@rated = rated
+@synopsis = synopsis
+@@all << self
+end
+
+
+end
